4a6d538395527f508ffb556eb5b5652f5468ec9a
Alina <dmitrium02@mail.ru>
2024-05-29 13:23:03 +0300

    install

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/brand_static/'
+        ? '/'
         : '/',
     devServer: {
         proxy: 'http://localhost:3003'
