5737ca77fa665131d3688c412c6d24b3a819b747
Alina <dmitrium02@mail.ru>
2024-05-29 16:16:23 +0300

    install

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const webSocketsUrl = process.env.NODE_ENV === 'production' ?
-    'wss://lti.skolkovo.ru/brand_ws/' :
+    // 'wss://lti.skolkovo.ru/brand_ws/' :
+    'ws://localhost:2003' :
     'ws://localhost:2003';
 module.exports = {
     webSocketsUrl
